682310a22cc96a3fc4662385959983bac4c63809
Yevgen Patiy on desktop <evgenpatiy@gmail.com>
2019-08-12 16:34:14 +0300

    implement correct login

--- src/main/java/com/gmail/yevgen/spring/MainView.java
+++ src/main/java/com/gmail/yevgen/spring/MainView.java
@@ -1,5 +1,12 @@
 package com.gmail.yevgen.spring;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gmail.yevgen.spring.domain.PersonRepository;
 import com.gmail.yevgen.spring.ui.SignUpView;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
@@ -17,6 +24,7 @@ import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
@@ -26,8 +34,11 @@ import com.vaadin.flow.server.PWA;
 @PWA(name = "Web days calculator", shortName = "dayscalc", iconPath = "../frontend/img/logo.png")
 public class MainView extends VerticalLayout {
     private static final long serialVersionUID = 7657167124498205619L;
+    private PersonRepository personRepository;
 
-    public MainView() {
+    @Autowired
+    public MainView(PersonRepository personRepository) {
+        this.personRepository = personRepository;
         Button signInButton = new Button(" Sign In", VaadinIcon.SIGN_IN.create(), e -> showLoginForm().setOpened(true));
         signInButton.setMaxWidth("10em");
         signInButton.getStyle().set("marginRight", "10px");
@@ -68,16 +79,34 @@ public class MainView extends VerticalLayout {
         LoginI18n i18n = LoginI18n.createDefault();
         i18n.setAdditionalInformation("Provide non-empty login and password");
         LoginOverlay login = new LoginOverlay();
-        // login.setAction("dayspanel");
-        login.setError(true);
         login.setForgotPasswordButtonVisible(false);
         login.setTitle(title);
         login.setDescription("How many days you're lived already");
         login.setI18n(i18n);
         login.addLoginListener(e -> {
-            login.close();
-            showWrongLoginNotification("User not found, please sign up first");
+            if (ifPersonWithLoginExists(e.getUsername())) {
+                if (ifPersonWithLoginAndPasswordExists(e.getUsername(), e.getPassword())) {
+                    login.close();
+                    UI.getCurrent().navigate("dayspanel",
+                            QueryParameters.simple(Stream.of(new SimpleEntry<>("user", e.getUsername()))
+                                    .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue))));
+                } else {
+                    showWrongLoginNotification("Wrong password for user " + e.getUsername());
+                    login.setEnabled(true);
+                }
+            } else {
+                login.close();
+                showWrongLoginNotification("User not found, please sign up first");
+            }
         });
         return login;
+    }
+
+    boolean ifPersonWithLoginExists(String login) {
+        return personRepository.findByLogin(login) != null;
+    }
+
+    boolean ifPersonWithLoginAndPasswordExists(String login, String password) {
+        return personRepository.findByLoginAndPassword(login, password) != null;
     }
 }
